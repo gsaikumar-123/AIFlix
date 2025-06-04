43a2d820a0c6999281044c0589510e09823d318a
G SAI KUMAR <gsaikumar2505@gmail.com>
2025-06-04 13:51:48 +0530

    Add routing and UI components; integrate Tailwind CSS and React Router

---
+++ aiflix/src/components/Login.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div className="relative h-screen w-full">
+      <img
+        className="absolute top-0 left-0 w-full h-full object-cover scale-125 -z-10"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/af2fac72-d956-4952-8686-4d45d359d78c/web/IN-en-20250526-TRIFECTA-perspective_5db3e163-56f7-47c7-9a65-b79b9d76bf24_large.jpg"
+        alt="bg"
+      />
+      <div className="absolute top-0 left-0 w-full h-full scale-125 bg-black opacity-60 -z-0" />
+      <Header />
+      <div className="flex justify-center items-center h-screen z-10 relative m-auto">
+        <div className="bg-black bg-opacity-75 rounded-md w-full max-w-md py-12 px-16">
+          <h1 className="text-white text-3xl font-bold mb-8">Sign In</h1>
+
+          <form className="flex flex-col space-y-5">
+            <input
+              className="p-4 bg-transparent bg-gray-50 bg-opacity-5 text-white rounded placeholder-gray-400 focus:ring-2 focus:ring-white border"
+              type="text"
+              placeholder="Email or phone number"
+            />
+            <input
+              className="p-4 bg-transparent bg-gray-50 bg-opacity-5 text-white rounded placeholder-gray-400 focus:ring-2 focus:ring-white border"
+              type="password"
+              placeholder="Password"
+            />
+            <button className="bg-red-600 text-white py-2 rounded font-semibold hover:bg-red-700 transition">
+              Sign In
+            </button>
+            <h1 className='text-gray-400 text-center'>OR</h1>
+            <button className="text-white py-2 rounded font-semibold bg-gray-50 bg-opacity-20">
+              Use a sign-in code
+            </button>
+            <a href="/forget"className='text-white text-center underline'>Forgot password?</a>
+
+            <div className="flex items-center text-gray-400 gap-3">
+                <input type="checkbox" className="form-checkbox size-4" />
+                <p className='text-white text-[16px]'>Remember me</p>
+            </div>
+
+            <p className="text-gray-400">
+                New to Netflix?{' '}
+                <span className="text-white hover:underline cursor-pointer">Sign up now</span>.
+            </p>
+          </form>
+
+          <p className="text-xs text-gray-500 mt-4">
+            This page is protected by Google reCAPTCHA to ensure you're not a bot.{' '}
+            <span className="text-blue-500 hover:underline cursor-pointer">Learn more.</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
