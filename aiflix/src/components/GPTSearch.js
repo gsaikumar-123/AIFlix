cdc1bed2ca6e926855e0a8b926264e81a1ad284e
G SAI KUMAR <gsaikumar2505@gmail.com>
2025-06-15 20:39:48 +0530

    Replace placeholder text with input field in GPTSearch component

--- aiflix/src/components/GPTSearch.js
+++ aiflix/src/components/GPTSearch.js
@@ -3,7 +3,11 @@ import React from 'react'
 const GPTSearch = () => {
   return (
     <div>
-      GPT Search Component
+      <input
+          className="p-4 bg-transparent bg-gray-50 bg-opacity-5 text-white rounded placeholder-gray-400 focus:ring-2 focus:ring-white border"
+          type="text"
+          placeholder="Enter Message"
+      />
     </div>
   )
 }
